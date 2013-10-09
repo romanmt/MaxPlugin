2804639dd7b08e26f417b1b0334b5d8b415b6c6c
Matt Roman <romanmt@gmail.com>
2013-10-09 11:46:45 -0400

    prints a log message to the server log

---
+++ src/com/romanmt/maxplugin/MaxPlugin.java
@@ -0,0 +1,10 @@
+package com.romanmt.maxplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaxPlugin extends JavaPlugin {
+	@Override 	
+	public void onEnable() {
+		getLogger().info("BUDDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	}
+}
