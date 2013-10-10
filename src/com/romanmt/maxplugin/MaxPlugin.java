84e8d99a2062ce73828389f768e31ffd2b46a5f5
Matt Roman <romanmt@gmail.com>
2013-10-10 08:01:03 -0400

    Adds build command, not yet tested with bukkit 1.6.2

--- src/com/romanmt/maxplugin/MaxPlugin.java
+++ src/com/romanmt/maxplugin/MaxPlugin.java
@@ -1,10 +1,66 @@
 package com.romanmt.maxplugin;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MaxPlugin extends JavaPlugin {
-	@Override 	
+	@Override
 	public void onEnable() {
 		getLogger().info("BUDDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
+
+	private void generateCube(Location loc, int length){
+	    // Set one corner of the cube to the given location.
+	    // Uses getBlockN() instead of getN() to avoid casting to an int later.
+	    int x1 = loc.getBlockX();
+	    int y1 = loc.getBlockY();
+	    int z1 = loc.getBlockZ();
+
+	    // Figure out the opposite corner of the cube by taking the corner and adding length to all coordinates.
+	    int x2 = x1 + length;
+	    int y2 = y1 + length;
+	    int z2 = z1 + length;
+
+	    World world = loc.getWorld();
+	    // Loop over the cube in the x dimension.
+	    for (int xPoint = x1; xPoint <= x2; xPoint++) {
+		// Loop over the cube in the y dimension.
+		for (int yPoint = y1; yPoint <= y2; yPoint++) {
+		    // Loop over the cube in the z dimension.
+		    for (int zPoint = z1; zPoint <= z2; zPoint++) {
+			// Get the block that we are currently looping over.
+			Block currentBlock = world.getBlockAt(xPoint, yPoint, zPoint);
+			// Set the block to type 57 (Diamond block!)
+			currentBlock.setType(Material.DIAMOND_BLOCK);
+		    }
+		}
+	    }
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		// TODO Auto-generated method stub
+		if(command.getName().equalsIgnoreCase("build")){
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+			} else {
+				Player player = (Player) sender;
+				Location loc = player.getLocation();
+				loc.setY(loc.getY() + 15);
+				generateCube(loc, 60);
+			}
+			return true;
+		} //If this has happened the function will return true.
+		// If this hasn't happened the a value of false will be returned.
+		return super.onCommand(sender, command, label, args);
+	}
+
+
 }
