2544f7a8c06078dad54fa1ae825f2fe184afa6ee
Matt Roman <romanmt@gmail.com>
2013-10-12 22:39:20 -0400

    Allows the user to select the size of the block they create

--- src/com/romanmt/maxplugin/MaxPlugin.java
+++ src/com/romanmt/maxplugin/MaxPlugin.java
@@ -15,32 +15,33 @@ public final class MaxPlugin extends JavaPlugin {
 		getLogger().info("BUDDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 
+
 	private void generateCube(Location loc, int length){
-	    // Set one corner of the cube to the given location.
-	    // Uses getBlockN() instead of getN() to avoid casting to an int later.
-	    int x1 = loc.getBlockX();
-	    int y1 = loc.getBlockY();
-	    int z1 = loc.getBlockZ();
+		// Set one corner of the cube to the given location.
+		// Uses getBlockN() instead of getN() to avoid casting to an int later.
+		int x1 = loc.getBlockX();
+		int y1 = loc.getBlockY();
+		int z1 = loc.getBlockZ();
 
-	    // Figure out the opposite corner of the cube by taking the corner and adding length to all coordinates.
-	    int x2 = x1 + length;
-	    int y2 = y1 + length;
-	    int z2 = z1 + length;
+		// Figure out the opposite corner of the cube by taking the corner and adding length to all coordinates.
+		int x2 = x1 + length;
+		int y2 = y1 + length;
+		int z2 = z1 + length;
 
-	    World world = loc.getWorld();
-	    // Loop over the cube in the x dimension.
-	    for (int xPoint = x1; xPoint <= x2; xPoint++) {
-		// Loop over the cube in the y dimension.
-		for (int yPoint = y1; yPoint <= y2; yPoint++) {
-		    // Loop over the cube in the z dimension.
-		    for (int zPoint = z1; zPoint <= z2; zPoint++) {
-			// Get the block that we are currently looping over.
-			Block currentBlock = world.getBlockAt(xPoint, yPoint, zPoint);
-			// Set the block to type 57 (Diamond block!)
-			currentBlock.setType(Material.DIAMOND_BLOCK);
-		    }
+		World world = loc.getWorld();
+		// Loop over the cube in the x dimension.
+		for (int xPoint = x1; xPoint <= x2; xPoint++) {
+			// Loop over the cube in the y dimension.
+			for (int yPoint = y1; yPoint <= y2; yPoint++) {
+				// Loop over the cube in the z dimension.
+				for (int zPoint = z1; zPoint <= z2; zPoint++) {
+					// Get the block that we are currently looping over.
+					Block currentBlock = world.getBlockAt(xPoint, yPoint, zPoint);
+					// Set the block to type 57 (Diamond block!)
+					currentBlock.setType(Material.DIAMOND_BLOCK);
+				}
+			}
 		}
-	    }
 	}
 
 	@Override
@@ -54,7 +55,13 @@ public final class MaxPlugin extends JavaPlugin {
 				Player player = (Player) sender;
 				Location loc = player.getLocation();
 				loc.setY(loc.getY() + 15);
-				generateCube(loc, 60);
+				int size = 10;
+				if(args.length > 0)
+					size = Integer.parseInt(args[0]);
+				getLogger().info("cube size: " + size);
+				generateCube(loc, size);
+				// do something
+
 			}
 			return true;
 		} //If this has happened the function will return true.
